5989a690582a0a63484ebb6e986ad0a1c8c91877
Stephen C. Austin <stephen.austin@pwnt.co>
2016-03-05 16:12:01 -0500

    wip

--- demo/index.js
+++ demo/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-const app = document.getElementsByClassName('demonstration')[0];
+const app = document.getElementsByClassName('app')[0];
 
 ReactDOM.render(<App />, app);
 
