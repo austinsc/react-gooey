04db1ce4221e77165ffae07ed21f9dbacb67351b
Stephen C. Austin <stephen.austin@pwnt.co>
2016-08-13 13:59:27 -0400

    updated

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-[![build status](https://secure.travis-ci.org/survivejs/react-component-boilerplate.png)](http://travis-ci.org/survivejs/react-component-boilerplate) [![bitHound Score](https://www.bithound.io/github/survivejs/react-component-boilerplate/badges/score.svg)](https://www.bithound.io/github/survivejs/react-component-boilerplate) [![Dependency Status](https://david-dm.org/survivejs/react-component-boilerplate.svg)](https://david-dm.org/survivejs/react-component-boilerplate)
+[![build status](https://secure.travis-ci.org/survivejs/react-component-boilerplate.svg)](http://travis-ci.org/survivejs/react-component-boilerplate) [![bitHound Score](https://www.bithound.io/github/survivejs/react-component-boilerplate/badges/score.svg)](https://www.bithound.io/github/survivejs/react-component-boilerplate) [![Dependency Status](https://david-dm.org/survivejs/react-component-boilerplate.svg)](https://david-dm.org/survivejs/react-component-boilerplate)
+
 # react-component-boilerplate - Boilerplate for React.js components
 
-This is a simple boilerplate that has been developed to make it easier to develop React components and small projects. Check out [SurviveJS - Webpack and React](http://survivejs.com/) to dig deeper into the topic.
+This is a simple boilerplate that has been developed to make it easier to develop React components and small projects.
+
+> Check out [SurviveJS - Webpack and React](http://survivejs.com/) to dig deeper into the topic.
 
 ## Basic Usage
 
@@ -13,21 +16,20 @@ If you want to replace project meta information (author etc.), consider using a 
 * Creating a version - **npm version <x.y.z>** - Updates */dist* and *package.json* with the new version and create a version tag to Git.
 * Publishing a version - **npm publish** - Pushes a new version to npm and updates the project site.
 
-If you don't want to use universal rendering for the React portion, set `RENDER_UNIVERSAL` to `false` at *webpack.config.babel.js*.
-
 ### Testing
 
 The test setup is based on Karma/Mocha/Chai/Phantom. Code coverage report is generated through istanbul/isparta to `build/`.
 
 * Running tests once - **npm test**
-* Running tests continuously **npm run test:tdd**
+* Running tests continuously - **npm run test:tdd**
+* Running individual tests - **npm test -- <pattern>** - Works with `test:tdd` too.
 * Linting - **npm run test:lint** - Runs ESLint.
 
-### Demo Site
+### Documentation Site
 
-The boilerplate includes a [GitHub Pages](https://pages.github.com/) specific portion for setting up a demo site for the component. The main commands handle with the details for you. Sometimes you might want to generate and deploy it by hand, or just investigate the generated bundle.
+The boilerplate includes a [GitHub Pages](https://pages.github.com/) specific portion for setting up a documentation site for the component. The main commands handle with the details for you. Sometimes you might want to generate and deploy it by hand, or just investigate the generated bundle.
 
-* Building - **npm run gh-pages** - Builds the demo into `./gh-pages` directory.
+* Building - **npm run gh-pages** - Builds the documentation into `./gh-pages` directory.
 * Deploying - **npm run deploy-gh-pages** - Deploys the contents of `./gh-pages` to the `gh-pages` branch. GitHub will pick this up automatically. Your site will be available through *<user name>.github.io/<project name>`.
 * Generating stats - **npm run stats** - Generates stats that can be passed to [webpack analyse tool](https://webpack.github.io/analyse/). This is useful for investigating what the build consists of.
 
--- karma.conf.js
+++ karma.conf.js
@@ -1,63 +1,102 @@
+/* eslint-disable */
 // Reference: http://karma-runner.github.io/0.13/config/configuration-file.html
 require('babel-register');
 
-module.exports = function karmaConfig (config) {
-    config.set({
-        frameworks: [
-            // Reference: https://github.com/karma-runner/karma-mocha
-            // Set framework to mocha
-            'mocha',
+module.exports = function karmaConfig(config) {
+  config.set({
+    frameworks: [
+      // Reference: https://github.com/karma-runner/karma-mocha
+      // Set framework to mocha
+      'mocha',
 
-            // Reference: http://chaijs.com/api/bdd/
-            // Use chai assertions
-            'chai'
-        ],
+      // Reference: http://chaijs.com/api/bdd/
+      // Use chai assertions
+      'chai'
+    ],
 
-        reporters: [
-            // Reference: https://github.com/mlex/karma-spec-reporter
-            // Set reporter to print detailed results to console
-            'spec',
+    client: {
+      args: parseTestPattern(process.argv),
+      mocha: {}
+    },
 
-            // Reference: https://github.com/karma-runner/karma-coverage
-            // Output code coverage files
-            'coverage'
-        ],
+    reporters: [
+      // Reference: https://github.com/mlex/karma-spec-reporter
+      // Set reporter to print detailed results to console
+      'spec',
 
-        files: [
-            // Reference: https://www.npmjs.com/package/phantomjs-polyfill
-            // Needed because React.js requires bind and phantomjs does not support it
-            'node_modules/phantomjs-polyfill/bind-polyfill.js',
+      // Reference: https://github.com/karma-runner/karma-coverage
+      // Output code coverage files
+      'coverage'
+    ],
 
-            // Grab all files in the tests directory that contain _test.
-            'tests/**/*_test.*'
-        ],
+    files: [
+      // Reference: https://www.npmjs.com/package/phantomjs-polyfill
+      // Needed because React.js requires bind and phantomjs does not support it
+      'node_modules/phantomjs-polyfill/bind-polyfill.js',
 
-        preprocessors: {
-            // Reference: http://webpack.github.io/docs/testing.html
-            // Reference: https://github.com/webpack/karma-webpack
-            // Convert files with webpack and load sourcemaps
-            'tests/**/*_test.*': ['webpack', 'sourcemap']
+      // Grab all files in the tests directory that contain _test.
+      'tests/**/*_test.*'
+    ],
+
+    preprocessors: {
+      // Reference: http://webpack.github.io/docs/testing.html
+      // Reference: https://github.com/webpack/karma-webpack
+      // Convert files with webpack and load sourcemaps
+      'tests/**/*_test.*': ['webpack', 'sourcemap'],
+      'src/**/*.*': 'coverage'
+    },
+
+    browsers: [
+      // Run tests using PhantomJS
+      'PhantomJS'
+    ],
+
+    singleRun: true,
+
+    // Configure code coverage reporter
+    coverageReporter: {
+      reporters: [
+        // generates ./coverage/lcov.info
+        {
+          type: 'lcovonly',
+          subdir: '.'
         },
-
-        browsers: [
-            // Run tests using PhantomJS
-            'PhantomJS'
-        ],
-
-        singleRun: true,
-
-        // Configure code coverage reporter
-        coverageReporter: {
-            dir: 'build/coverage/',
-            type: 'html'
+        // generates ./coverage/coverage-final.json
+        {
+          type: 'json',
+          subdir: '.'
         },
-
-        // Test webpack config
-        webpack: require('./webpack.config.babel'),
-
-        // Hide webpack build information from output
-        webpackMiddleware: {
-            noInfo: true
+        // generates ./coverage/index.html
+        {
+          type: 'html',
+          subdir: '.'
         }
-    });
+      ]
+    },
+
+    // Test webpack config
+    webpack: require('./webpack.config.babel'),
+
+    // Hide webpack build information from output
+    webpackMiddleware: {
+      noInfo: true
+    }
+  });
 };
+
+function parseTestPattern(argv) {
+  var found = false;
+  var pattern = argv.map(function(v) {
+    if (found) {
+      return v;
+    }
+
+    if (v === '--') {
+      found = true;
+    }
+  }).
+  filter(function(a) { return a }).
+  join(' ');
+
+  return pattern ? ['--grep', pattern] : [];
+}
