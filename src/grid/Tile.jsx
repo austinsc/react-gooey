2f6eec33ff0da4b516d335c9669c1424e48a297f
Emily Moore <emily.moore@rovisys.com>
2016-08-15 12:03:18 -0400

    Tile

---
+++ src/grid/Tile.jsx
@@ -0,0 +1,31 @@
+import React, {Component, PropTypes} from 'react';
+import classNames from 'classnames';
+import {GooeyPropTypes} from '../utils';
+
+export default class Tile extends Component {
+  static displayName = 'Tile';
+  static propTypes = {
+    ancestor: PropTypes.bool,
+    parent: PropTypes.bool,
+    child: PropTypes.bool,
+    vertical: PropTypes.bool,
+    children: PropTypes.any,
+    className: PropTypes.any,
+    size: GooeyPropTypes.column
+  };
+
+  render() {
+    const {ancestor, parent, child, vertical, children, className, size, ...rest} = this.props;
+    const classes = classNames('tile', {
+      'is-ancestor': ancestor,
+      'is-parent': parent,
+      'is-child': child,
+      'is-vertical': vertical,
+      [className]: !!className,
+      [`is-${size}`]: !!size
+    });
+    return (
+      <div className={classes} {...rest}>{children}</div>
+    );
+  }
+}
