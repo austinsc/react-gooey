b476c9bd3b548e062d835d8824575a2a101f721a
Stephen C. Austin <stephen.austin@pwnt.co>
2017-06-06 13:51:03 -0400

    wip docs layout

--- docs/Layout.jsx
+++ docs/Layout.jsx
@@ -1,8 +1,10 @@
 import React, {Component, PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import {Hero, Title, Container, Menu, MenuLabel, MenuList, Columns, Column} from "../src/index";
-import Subtitle from "../src/elements/Subtitle";
+import {Hero, Title, Container, Subtitle, Nav, Tabs, Tab} from "../src/index";
+import routes from './routes';
+
+const main = routes.map(x => ({title: x.title, path: x.path || (x.pages && x.pages[0] && x.pages[0].path)}));
 
 class Navigation extends Component {
   render() {
@@ -36,7 +38,23 @@ export default class Layout extends PureComponent {
               </Title>
             </Container>
           </Hero.Body>
+          <Hero.Foot>
+            <Container>
+              <Tabs boxed>
+                {main.map((x, i) => (
+                  <Tab key={x.title} href={`#${x.path}`} active={_.endsWith(window.location, x.path)}>
+                    {x.title}
+                  </Tab>
+                ))}
+              </Tabs>
+            </Container>
+          </Hero.Foot>
         </Hero>
+        <Container>
+          <Nav shadow>
+
+          </Nav>
+        </Container>
       </div>
     );
   }
